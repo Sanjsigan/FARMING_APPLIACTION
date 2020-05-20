51226f54900ec721d7177e9efbe793e3f10d6aca
Sanjsigan <sanjsi0507@gmail.com>
2020-05-20 17:59:59 +0530

    Going ON

--- FARMING_APPLICATION/resources/views/Home.blade.php
+++ FARMING_APPLICATION/resources/views/Home.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DashBoard</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
     <link href="http://demo.interface.club/limitless/demo/Template/global_assets/css/icons/icomoon/styles.min.css" type="text/css">
     <link href="http://demo.interface.club/limitless/demo/Template/layout_1/LTR/default/full/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
@@ -50,7 +53,7 @@
 <div class="navbar navbar-expand-md navbar-dark">
 		<div class="navbar-brand">
 			<a href="index.html" class="d-inline-block">
-				<img src="../../../../global_assets/images/logo_light.png" alt="">
+				<img src="/public/img/logo_light.png" alt="">
 			</a>
 		</div>
 
@@ -71,87 +74,6 @@
 					</a>
 				</li>
 
-				<li class="nav-item dropdown">
-					<a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-						<i class="icon-git-compare"></i>
-						<span class="d-md-none ml-2">Git updates</span>
-						<span class="badge badge-pill bg-warning-400 ml-auto ml-md-0">9</span>
-					</a>
-
-					<div class="dropdown-menu dropdown-content wmin-md-350">
-						<div class="dropdown-content-header">
-							<span class="font-weight-semibold">Git updates</span>
-							<a href="#" class="text-default"><i class="icon-sync"></i></a>
-						</div>
-
-						<div class="dropdown-content-body dropdown-scrollable">
-							<ul class="media-list">
-								<li class="media">
-									<div class="mr-3">
-										<a href="#" class="btn bg-transparent border-primary text-primary rounded-round border-2 btn-icon"><i class="icon-git-pull-request"></i></a>
-									</div>
-
-									<div class="media-body">
-										Drop the IE <a href="#">specific hacks</a> for temporal inputs
-										<div class="text-muted font-size-sm">4 minutes ago</div>
-									</div>
-								</li>
-
-								<li class="media">
-									<div class="mr-3">
-										<a href="#" class="btn bg-transparent border-warning text-warning rounded-round border-2 btn-icon"><i class="icon-git-commit"></i></a>
-									</div>
-
-									<div class="media-body">
-										Add full font overrides for popovers and tooltips
-										<div class="text-muted font-size-sm">36 minutes ago</div>
-									</div>
-								</li>
-
-								<li class="media">
-									<div class="mr-3">
-										<a href="#" class="btn bg-transparent border-info text-info rounded-round border-2 btn-icon"><i class="icon-git-branch"></i></a>
-									</div>
-
-									<div class="media-body">
-										<a href="#">Chris Arney</a> created a new <span class="font-weight-semibold">Design</span> branch
-										<div class="text-muted font-size-sm">2 hours ago</div>
-									</div>
-								</li>
-
-								<li class="media">
-									<div class="mr-3">
-										<a href="#" class="btn bg-transparent border-success text-success rounded-round border-2 btn-icon"><i class="icon-git-merge"></i></a>
-									</div>
-
-									<div class="media-body">
-										<a href="#">Eugene Kopyov</a> merged <span class="font-weight-semibold">Master</span> and <span class="font-weight-semibold">Dev</span> branches
-										<div class="text-muted font-size-sm">Dec 18, 18:36</div>
-									</div>
-								</li>
-
-								<li class="media">
-									<div class="mr-3">
-										<a href="#" class="btn bg-transparent border-primary text-primary rounded-round border-2 btn-icon"><i class="icon-git-pull-request"></i></a>
-									</div>
-
-									<div class="media-body">
-										Have Carousel ignore keyboard events
-										<div class="text-muted font-size-sm">Dec 12, 05:46</div>
-									</div>
-								</li>
-							</ul>
-						</div>
-
-						<div class="dropdown-content-footer bg-light">
-							<a href="#" class="text-grey mr-auto">All updates</a>
-							<div>
-								<a href="#" class="text-grey" data-popup="tooltip" title="Mark all as read"><i class="icon-radio-unchecked"></i></a>
-								<a href="#" class="text-grey ml-2" data-popup="tooltip" title="Bug tracker"><i class="icon-bug2"></i></a>
-							</div>
-						</div>
-					</div>
-				</li>
 			</ul>
 
 			<span class="badge bg-success ml-md-3 mr-md-auto">Online</span>
@@ -159,7 +81,7 @@
 			<ul class="navbar-nav">
 				<li class="nav-item dropdown">
 					<a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-						<i class="icon-people"></i>
+					<i class="fa fa-child" style="font-size:36px"></i>
 						<span class="d-md-none ml-2">Users</span>
 					</a>
 
@@ -237,7 +159,7 @@
 
 				<li class="nav-item dropdown">
 					<a href="#" class="navbar-nav-link dropdown-toggle caret-0" data-toggle="dropdown">
-						<i class="icon-bubbles4"></i>
+                    <i style="font-size:24px" class="fa">&#xf0a2;</i>
 						<span class="d-md-none ml-2">Messages</span>
 						<span class="badge badge-pill bg-warning-400 ml-auto ml-md-0">2</span>
 					</a>

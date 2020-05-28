b25b7625a3c836e4e3e6225ca7ef81dceff1ca55
Sanjsigan <sanjsi0507@gmail.com>
2020-05-28 20:43:23 +0530

    start to add new requirements

--- FARMING_APPLICATION/resources/views/Home.blade.php
+++ FARMING_APPLICATION/resources/views/Home.blade.php
@@ -41,6 +41,7 @@
 	<script src="http://demo.interface.club/limitless/demo/Template/global_assets/js/demo_charts/pages/dashboard/light/heatmaps.js"></script>
 	<script src="http://demo.interface.club/limitless/demo/Template/global_assets/js/demo_charts/pages/dashboard/light/pies.js"></script>
 	<script src="http://demo.interface.club/limitless/demo/Template/global_assets/js/demo_charts/pages/dashboard/light/bullets.js"></script>
+	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
 	<!-- /theme JS files -->
 
 
@@ -75,7 +76,7 @@
 				</li>
 
 			</ul>
-
+      
 			<span class="badge bg-success ml-md-3 mr-md-auto">Online</span>
 
 			<ul class="navbar-nav">
@@ -328,7 +329,7 @@
 		<ul class="nav nav-sidebar" data-nav-type="accordion">
 
 			<!-- Main -->
-			<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Main</div> <i class="icon-menu" title="Main"></i></li>
+			<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">User</div> <i class="icon-menu" title="Main"></i></li>
 			<li class="nav-item">
 				<a href="index.html" class="nav-link active">
 					<i class="icon-home4"></i>
@@ -337,76 +338,30 @@
 					</span>
 				</a>
 			</li>
-			<li class="nav-item nav-item-submenu">
-				<a href="#" class="nav-link"><i class="icon-copy"></i> <span>Layouts</span></a>
-
-				<ul class="nav nav-group-sub" data-submenu-title="Layouts">
-					<li class="nav-item"><a href="index.html" class="nav-link active">Default layout</a></li>
-					<li class="nav-item"><a href="../../../../layout_2/LTR/default/full/index.html" class="nav-link">Layout 2</a></li>
-					<li class="nav-item"><a href="../../../../layout_3/LTR/default/full/index.html" class="nav-link">Layout 3</a></li>
-					<li class="nav-item"><a href="../../../../layout_4/LTR/default/full/index.html" class="nav-link">Layout 4</a></li>
-					<li class="nav-item"><a href="../../../../layout_5/LTR/default/full/index.html" class="nav-link">Layout 5</a></li>
-					<li class="nav-item"><a href="../../../../layout_6/LTR/default/full/index.html" class="nav-link disabled">Layout 6 <span class="badge bg-transparent align-self-center ml-auto">Coming soon</span></a></li>
-				</ul>
-			</li>
-			<li class="nav-item nav-item-submenu">
-				<a href="#" class="nav-link"><i class="icon-color-sampler"></i> <span>Themes</span></a>
-
-				<ul class="nav nav-group-sub" data-submenu-title="Themes">
-					<li class="nav-item"><a href="index.html" class="nav-link active">Default</a></li>
-					<li class="nav-item"><a href="../../../LTR/material/full/index.html" class="nav-link">Material</a></li>
-					<li class="nav-item"><a href="../../../LTR/dark/full/index.html" class="nav-link">Dark</a></li>
-					<li class="nav-item"><a href="../../../LTR/clean/full/index.html" class="nav-link disabled">Clean <span class="badge bg-transparent align-self-center ml-auto">Coming soon</span></a></li>
-				</ul>
-			</li>
-			<li class="nav-item nav-item-submenu">
-				<a href="#" class="nav-link"><i class="icon-stack"></i> <span>Starter kit</span></a>
-
-				<ul class="nav nav-group-sub" data-submenu-title="Starter kit">
-					<li class="nav-item"><a href="../seed/layout_nav_horizontal.html" class="nav-link">Horizontal navigation</a></li>
-					<li class="nav-item"><a href="../seed/sidebar_none.html" class="nav-link">No sidebar</a></li>
-					<li class="nav-item"><a href="../seed/sidebar_main.html" class="nav-link">1 sidebar</a></li>
-					<li class="nav-item nav-item-submenu">
-						<a href="#" class="nav-link">2 sidebars</a>
-						<ul class="nav nav-group-sub">
-							<li class="nav-item"><a href="../seed/sidebar_secondary.html" class="nav-link">Secondary sidebar</a></li>
-							<li class="nav-item"><a href="../seed/sidebar_right.html" class="nav-link">Right sidebar</a></li>
-						</ul>
-					</li>
-					<li class="nav-item nav-item-submenu">
-						<a href="#" class="nav-link">3 sidebars</a>
-						<ul class="nav nav-group-sub">
-							<li class="nav-item"><a href="../seed/sidebar_right_hidden.html" class="nav-link">Right sidebar hidden</a></li>
-							<li class="nav-item"><a href="../seed/sidebar_right_visible.html" class="nav-link">Right sidebar visible</a></li>
-						</ul>
-					</li>
-					<li class="nav-item nav-item-submenu">
-						<a href="#" class="nav-link">Content sidebars</a>
-						<ul class="nav nav-group-sub">
-							<li class="nav-item"><a href="../seed/sidebar_content_left.html" class="nav-link">Left sidebar</a></li>
-							<li class="nav-item"><a href="../seed/sidebar_content_right.html" class="nav-link">Right sidebar</a></li>
-						</ul>
-					</li>
-					<li class="nav-item"><a href="../seed/layout_boxed.html" class="nav-link">Boxed layout</a></li>
-					<li class="nav-item-divider"></li>
-					<li class="nav-item"><a href="../seed/navbar_fixed_main.html" class="nav-link">Fixed main navbar</a></li>
-					<li class="nav-item"><a href="../seed/navbar_fixed_secondary.html" class="nav-link">Fixed secondary navbar</a></li>
-					<li class="nav-item"><a href="../seed/navbar_fixed_both.html" class="nav-link">Both navbars fixed</a></li>
-					<li class="nav-item"><a href="../seed/layout_fixed.html" class="nav-link">Fixed layout</a></li>
-				</ul>
-			</li>
 			<li class="nav-item">
-				<a href="changelog.html" class="nav-link">
-					<i class="icon-list-unordered"></i>
-					<span>Changelog</span>
-					<span class="badge bg-blue-400 align-self-center ml-auto">2.3</span>
+				<a href="#" class="nav-link active ">
+				<i class='fas fa-home'></i>
+					<span>
+						About Us...
+					</span>
 				</a>
 			</li>
-			<li class="nav-item"><a href="../../../RTL/default/full/index.html" class="nav-link"><i class="icon-width"></i> <span>RTL version</span></a></li>
+			<li class="nav-item nav-item-submenu">
+				<a href="#" class="nav-link "><i class="icon-copy"></i> <span>Main Part</span></a>
+
+				<ul class="nav nav-group-sub" data-submenu-title="Layouts">
+					<li class="nav-item"><a href="#" class="nav-link active">Details of Seeds</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Detils of Soil</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Details of water flow methods</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Details of Crop Insecticide</a></li>
+					
+				</ul>
+			</li>
+
 			<!-- /main -->
 
 			<!-- Forms -->
-			<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Forms</div> <i class="icon-menu" title="Forms"></i></li>
+			<li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">User Section...</div> <i class="icon-menu" title="Forms"></i></li>
 			<li class="nav-item nav-item-submenu">
 				<a href="#" class="nav-link"><i class="icon-pencil3"></i> <span>Form components</span></a>
 				<ul class="nav nav-group-sub" data-submenu-title="Form components">
